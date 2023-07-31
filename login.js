457f356629d57928f121b7c938122803b9c67776
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-01 05:05:36 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 var xhr = new XMLHttpRequest();
 
 xhr.onload = reqListener; 
-xhr.open("GET", "https://effecthouse.tiktok.com/passport/web/gen_pkce_verify_ticket/?aid=1459&code_challenge=1445d85618e42015ca006df50b1af7ef&language=en");
+xhr.open("GET", "https://effecthouse.tiktok.com/passport/web/gen_pkce_verify_ticket/?aid=1459&code_challenge=f3ab76f534c5d5d83ea6988bdbdbe505&language=en");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 xhr.send();

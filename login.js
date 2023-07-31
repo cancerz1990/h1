eaa603311194385df6af49ce365ad5070625a191
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-07-31 23:43:53 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 xhr.onload = reqListener; 
-xhr.open("POST", "https://www.tiktok.com/passport/web/scan_index/?aid=1459&append_common_params=1&hide_nav_bar=1&next_url=https%3A%2F%2Fwww.tiktok.com%2Fpassport%2Fweb%2Fscan_qrcode%2F&qr_source_aid=1459&token=zAe1UfD1xr72yDFNLmC8VzPnsnr0v3aV9HUd-F6RWQM%3D_sg1");
+xhr.open("GET", "https://www.tiktok.com/passport/web/scan_index/?aid=1459&append_common_params=1&hide_nav_bar=1&next_url=https%3A%2F%2Fwww.tiktok.com%2Fpassport%2Fweb%2Fscan_qrcode%2F&qr_source_aid=1459&token=zAe1UfD1xr72yDFNLmC8VzPnsnr0v3aV9HUd-F6RWQM%3D_sg1");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 xhr.send();

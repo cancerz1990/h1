5e405a772a5f9d8323bdb9c1c0f1557aa82df1b6
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-01 21:39:48 +0700

    Update login1.js

--- login1.js
+++ login1.js
@@ -1,7 +1,7 @@
 var xhr = new XMLHttpRequest();
 
 xhr.onload = reqListener; 
-xhr.open("POST", "//app.kubernetes.io/");
+xhr.open("POST", "tcp://app.kubernetes.io/");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send();

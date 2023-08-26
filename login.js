bde8414a2a2d067d25d0ab52fde79f8e71a1fd76
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-26 17:46:25 +0700

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,8 @@ var req = new XMLHttpRequest();
 req.onload = reqListener;
 req.open('POST','https://ads.tiktok.com/passport/web/shark/safe_verify/?msToken=or6IAX2oBn8DiZB3AnCIeMAsJoeVSdb5TsIH7rHyFtYUl6Q4XwdvxpkmvqRbpUPfC29MiWfYR2v5RAFkkTxbhUoqb-__trxRjq2aw8mQ6sfypZkHddXtL_gIrSRYylxl6A8oyyMndkUMH8-3_A==&X-Bogus=DFSzsIVLZC0tgY/HtxxIuUWdbTo7&_signature=_02B4Z6wo00001b4nHjwAAIDBlF7mqG44QVW-JhqAAAth27',true);
 req.withCredentials = true;
-req.setRequestHeader('Content-Type', 'text/plain',true);
+req.setRequestHeader('Content-Type', 'application/json',true);
+req.setRequestHeader('Referer', 'https://ads.tiktok.com',true);
 req.send('aid=1583&service=&language=en&target=bind_oauth',true);
 
 function reqListener() {

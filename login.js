398686309e4a03121d283ce403d2c698c6839d0b
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-31 04:59:03 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 xhr.onload = reqListener; 
-xhr.open("GET", "https://webcast.tiktok.com/webcast/wallet_api_tiktok/withdraw/preference_config/?aid=1284&account_type=102");
+xhr.open("GET", "https://ads.tiktok.com/ac/api/v1/oauth/tt_oauth_base_info?msToken=-_CmeTG22eMpPpPCPh9BY3_yqAm-eiEPFz3xPakAibImUSqX0eZQ4ECjF4f9LrTnmehKefiM4G57xjo1vJVdolPrNSmw6UrRj4jJESVJe6Nn6xIcg9Bt_z-QfJ9bxMPpH9earg4U8bolGK21dMM=&X-Bogus=DFSzsIVLnOTANV/HtxasStWdbTbA&_signature=_02B4Z6wo00001.2zvDAAAIDD18pEpBGo89f9sriAAJuQa8");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send();

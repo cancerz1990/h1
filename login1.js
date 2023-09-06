b8c4908475192b6c57622f407ddcdac14ec103a6
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-09-07 03:57:40 +0700

    Update login1.js

--- login1.js
+++ login1.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 xhr.onload = reqListener; 
-xhr.open("GET", "https://fp.us.tiktokv.com/check_login/?aid=1583&service=ads.tiktok.com&language=en&appId=1583&lang=en");
+xhr.open("GET", "https://api.tiktokv.com/check_login/?aid=1583&service=ads.tiktok.com&language=en&appId=1583&lang=en");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send(null);

